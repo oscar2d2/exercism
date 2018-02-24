a7af20c9872bb616d655ea199fb397ffa07aeb99
oscar2d2 <oscarkong@gmail.com>
2018-02-24 10:01:12 +0800

    Reduces the line of codes and change package comment

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,18 +1,7 @@
-// Check if a year is leap year or not
+// Package leap if a year is leap year or not
 package leap
 
 // IsLeapYear takes a int for year and return a bool to indicate if the year is a leap year
 func IsLeapYear(year int) bool {
-
-	if year%4 == 0 {
-		if year%100 == 0 {
-			if year%400 == 0 {
-				return true
-			}
-			return false
-		}
-		return true
-	}
-
-	return false
+	return year%400 == 0 || (year%100 != 0 && year%4 == 0)
 }
